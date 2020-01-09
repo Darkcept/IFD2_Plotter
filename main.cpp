05ae59f49fa7aac1da672440ce78da93818b4637
Oncle Picsou <flavargues@users.noreply.github.com>
2020-01-09 17:37:41 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@
 //#define topicX "/plotter/posX"
 //#define topicY "/plotter/posY"
 
-#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <math.h>
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 //proto
@@ -27,16 +27,17 @@ void loop();
 
 #define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
 #define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 #define NEUTRE 375
 #define NEUTREM 1500
 #define VITESSE 1
 #define SERVO3MIN 200
-#define SERVO3MAX 380
-#define SERVO2MIN 322
-#define SERVO2MAX 480
-#define SERVO1MIN
-#define SERVO1MAX
+#define SERVO3MAX 600//x
+
+#define SERVO2MIN 300
+#define SERVO2MAX 480//y
+
+#define SERVO1MIN 184
+#define SERVO1MAX 232//z
 
 #include <Arduino.h>
 
@@ -45,7 +46,7 @@ char pos[4];
 int move;
 int speed=10; //vitesse moteur
 
-uint8_t servonum1 = 0;
+uint8_t servonum1 = 5;
 uint8_t servonum2 = 4;
 uint8_t servonum3 = 3;
 int joysticky1 = 34;
@@ -58,10 +59,10 @@ int pos1=NEUTRE,pos2=NEUTRE,pos3=NEUTRE;
 int clic=1;
 int pos33;
 
-const char* ssid = "AndroidM";
-const char* password = "fpjj5481";
+const char* ssid = "Crunch LAB";
+const char* password = "90xV@FsT";
 
-const char* mqtt_server = "192.168.43.137";
+const char* mqtt_server = "192.168.1.15";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -94,12 +95,6 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
-/*X-0.5 Y0.
-Z0.1
-G01 Z0. F5.
-G02 X0. Y0.5 I0.5 J0. F2.5*/
-int mode=-1;
-
 
 int draw(String instruct)
 {
@@ -108,33 +103,31 @@ int draw(String instruct)
     Serial.println(instruct);
     if (instruct=="G00") {
       speed=50; //vitesse rapide
-      Serial.println("Gotta go fast");
+      //Serial.println("Gotta go fast");
     } else if (instruct=="G01") {
       speed=10; //vitesse ecriture
-      Serial.println("Gotta go slow");
+      //Serial.println("Gotta go slow");
     } else {
       instruct.toCharArray(pos,5);
       switch (pos[0]) {
       case 'X':
       move=atoi(&pos[1]);
-      Serial.println("Move X");
       Serial.println(move);
-        pwm.setPWM(servonum3, 0, (move*380)/24-200);
+        pwm.setPWM(servonum3,0,map(move,0,240,SERVO3MIN,SERVO3MAX));
         break;
       case 'Y':
       move=atoi(&pos[1]);
-      Serial.println("Move Y");
       Serial.println(move);
-        pwm.setPWM(servonum2, 0, (move*480)/32-322);
+        pwm.setPWM(servonum2,0,map(move,0,320,SERVO2MIN,SERVO2MAX));
         break;
       case 'Z':
       move=atoi(&pos[1]);
         if(move==0) {
           Serial.println("Stylo baissé");
-          //pwm.setPWM(servonum1, 0, );
+          pwm.setPWM(servonum1, 0, SERVO1MAX);
         } else if(move!=0) {
           Serial.println("Stylo remonté");
-          //pwm.setPWM(servonum1, 0, );
+          pwm.setPWM(servonum1, 0, SERVO1MIN);
         }
         break;
       default:
@@ -143,7 +136,6 @@ int draw(String instruct)
     }
   return success;
 }
-
 
 void callback(char* topic, byte* message, unsigned int length)
 {
@@ -163,9 +155,6 @@ void callback(char* topic, byte* message, unsigned int length)
 
   if(temp==1) 
   {
-    Serial.print("drew | ");
-    Serial.print(messageTemp);
-    Serial.println();
     client.publish(topicOut, "continue");
   }
   else
@@ -181,7 +170,7 @@ void setup()
   pinMode (joysticky1, INPUT) ;
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz  
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
   delay(10);
 
   Serial.begin(9600);  
@@ -193,26 +182,13 @@ void setup()
   pinMode(ledPin, OUTPUT);
 }
 
-void setServoPulse(uint8_t n,double pulse) {
-  double pulselength;
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert input seconds to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
-
 void reconnect()
 {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client"))
+    if (client.connect("Plotter"))
     {
       Serial.println("connected");
       // Subscribe
